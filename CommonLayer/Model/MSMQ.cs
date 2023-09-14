acba59e32f2138bc0d6bba592a0b3f038cca209b
Yashaswini192 <yashu19052@gmail.com>
2023-09-14 22:36:21 +0530

    [Yashaswini] Add:Implemented Model for User API

---
+++ CommonLayer/Model/MSMQ.cs
@@ -0,0 +1,62 @@
+﻿using Experimental.System.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class MSMQ
+    {
+        MessageQueue message = new MessageQueue();
+
+      
+        public void SendMessage(string token)
+        {
+           
+            message.Path = @".\Private$\Token";
+            try
+            {
+                if (!MessageQueue.Exists(message.Path))
+                {
+                    MessageQueue.Create(message.Path);
+                }
+                message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+                message.ReceiveCompleted += MessageQueue_RecieveCompleted;
+                message.Send(token);
+                message.BeginReceive();
+                message.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void MessageQueue_RecieveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            try
+            {
+                var msg = message.EndReceive(e.AsyncResult);
+                string token = msg.Body.ToString();
+                string subject = "Google Keep Reset Password Link";
+                var SMTP = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587, //for gmail
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential("yashu19052@gmail.com", "zwjctqcbkqcrpytj"),
+                };
+
+                SMTP.Send("yashu19052@gmail.com", "yashaswinivoruganti@gmail.com", subject, token);
+                message.BeginReceive();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
